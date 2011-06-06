a586fd40849053063b17bf04535f9b4f103c6772
Joao Eduardo Luis <jecluis@gmail.com>
2011-06-06 21:03:38 +0100

    modifications to the builder script

--- build-txbtrfs.sh
+++ build-txbtrfs.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 build_dir=/usr/src/builds/btrfs-unstable
-cur_dir=`pwd`;
+src_dir=/usr/src/btrfs-unstable;
 
 ret=0;
 
-cd /usr/src/btrfs-unstable;
+cur_dir=`pwd`;
+if [ $# -gt 0 ]; then
+	if [ $# -ne 2 ]; then
+		echo "usage: $0 [<src dir> <build dir>]"
+		exit 1;
+	fi
+	src_dir=$1
+	build_dir=$2
+	echo "Using $build_dir as build dir. 3 secs to abort."
+	sleep 2
+fi
+
+cd $src_dir
 sudo make O=$build_dir M=fs/btrfs
 
 if [ $? -ne 0 ]; then
